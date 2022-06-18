c37e8126ee261d6b345e2cb2d5a8e43e9976bb92
SspidesS <99414766+SspidersS@users.noreply.github.com>
2022-06-18 17:03:26 +0200

    .

---
+++ exercise35.java
@@ -0,0 +1,155 @@
+/*
+Напиши метод public int getPricesSum(int[] prices, int minPrice, int maxPrice).
+Он принимает список цен, и суммирует их, включая лишь те, которые больше чем minPrice (включительно)
+и меньше чем maxPrice (включительно)и возвращает сумму.
+
+Примеры тестов
+вызов getPricesSum(new int[] {10, 700, 50, 500}, 10, 50) возвращает 60
+ */
+
+public class exercise35 {
+    public int getPricesSum(int[] prices, int minPrice, int maxPrice) {
+        int sum = 0;
+
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < maxPrice && prices[i] > minPrice) {
+                sum += prices[i];
+            }
+        }
+        return sum;
+    }
+
+
+    public void printPrices(float[] prices) {
+        for(float price: prices) {
+            System.out.println(price + " jup.");
+        }
+    }
+
+    public void multiplyPrices(float[] prices) {
+        for(int i = 0; i < prices.length; i++) {
+            float price = prices[i];
+
+            if (price < 1000) {
+                price *= 2f;
+            } else {
+                price *= 1.5f;
+            }
+
+            prices[i] = price;
+        }
+    }
+
+    public int[] findMinMaxPrices(int[] prices) {
+        if (prices.length == 0) {
+            return new int[0];
+        }
+
+        int min = prices[0];
+        int max = prices[0];
+
+        for(int price: prices) {
+            if (price > max) {
+                max = price;
+            }
+
+            if (price < min) {
+                min = price;
+            }
+        }
+
+        if (min == max) {
+            return new int[] {min};
+        }
+
+        return new int[] {min, max};
+    }
+
+    public int getMinPriceCount(int[] prices) {
+        if (prices.length == 0) {
+            return 0;
+        }
+
+        int min = prices[0];
+        for(int i = 0; i < prices.length; i++) {
+            if (prices[i] < min) {
+                min = prices[i];
+            }
+        }
+
+        int result = 0;
+        for(int i = 0; i < prices.length; i++) {
+            if (prices[i] == min) {
+                result++;
+            }
+        }
+
+        return result;
+    }
+
+    public int[] removePrice(int[] prices, int toRemove) {
+        int toRemoveCount = 0;
+        for(int price: prices) {
+            if (price == toRemove) {
+                toRemoveCount++;
+            }
+        }
+
+        int[] result = new int[prices.length - toRemoveCount];
+        int index = 0;
+        for(int price: prices) {
+            if (price != toRemove) {
+                result[index] = price;
+                index++;
+            }
+        }
+
+        return result;
+    }
+
+    public int[] leavePrice9(int[] prices) {
+        int validPriceCount = 0;
+        for(int price: prices) {
+            if (price%10 == 9) {
+                validPriceCount++;
+            }
+        }
+
+        int[] result = new int[validPriceCount];
+        int index = 0;
+        for(int price: prices) {
+            if (price%10 == 9) {
+                result[index] = price;
+                index++;
+            }
+        }
+
+        return result;
+    }
+
+    public String[] mergeStocks(String[] showcaseStocks, String[] warehouseStocks) {
+        String[] result = new String[showcaseStocks.length + warehouseStocks.length];
+
+        int index = 0;
+        for(String stock: showcaseStocks) {
+            result[index] = stock;
+            index++;
+        }
+
+        for(String stock: warehouseStocks) {
+            result[index] = stock;
+            index++;
+        }
+
+        return result;
+    }
+
+    //Test output
+    public static void main(String[] args) {
+        UberShop shop = new UberShop();
+
+        //Should be 144 - 20 + 50 + 40 + 34
+        int[] prices = new int[] {10, 20, 50, 40, 34, 500};
+        System.out.println(shop.getPricesSum(prices, 20, 50));
+    }
+}
